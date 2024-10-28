99b3fa6c4b26068ba59377f197a4c402e463cdf5
Lucas <110315297+Lucasciel@users.noreply.github.com>
2024-10-28 20:53:23 -0300

    Feat:Material Ui insert

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { darkTheme, GlobalStyle } from './styles/'
+import { darkTheme, GlobalStyle, lighTheme } from './styles/'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={lighTheme}>
       <GlobalStyle />
       <App />
     </ThemeProvider>
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,19 +1,27 @@
-import styled from 'styled-components'
+import { Grid, Box, Container } from '@mui/material'
+import { BannerImage } from '@/components'
 
-const RegistrationArea = styled.div`
-  background: #666;
-`
-const RegistrationImage = styled.div`
-  background-image: url(/login-image.svg);
-  background-size: cover;
-  height: 100vh;
-  width: 50vw;
-`
 function Registration() {
   return (
     <>
-      <RegistrationArea>Registration</RegistrationArea>
-      <RegistrationImage />
+      <Box>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            sx={{ alignItems: 'center', display: 'flex', height: '100vh' }}
+          >
+            <Container maxWidth="sm">
+              <h1>CADASTRO</h1>
+            </Container>
+          </Grid>
+
+          <Grid item sm={6} sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <BannerImage />
+          </Grid>
+        </Grid>
+      </Box>
     </>
   )
 }
