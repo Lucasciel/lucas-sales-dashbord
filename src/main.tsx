73e30a58b51181c3ab065e9a6c3df40c40f8bb5a
Lucas <110315297+Lucasciel@users.noreply.github.com>
2024-10-28 18:53:42 -0300

    Feat:create Themes 2

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { darkTheme, GlobalStyle, lighTheme } from './styles/'
+import { darkTheme, GlobalStyle } from './styles/'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
