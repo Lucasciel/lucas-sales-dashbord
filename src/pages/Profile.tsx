64fdb2debc070cdbc0b614fd31260e5c05cf05a2
Lucas <110315297+Lucasciel@users.noreply.github.com>
2024-11-05 14:55:26 -0300

    Revert "Revert 17 feat/profile request"

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,43 @@
+import { useContext } from 'react'
+import { CardComponent, StyledButton, StyledH2, Header } from '@/components'
+import { AppThemeContext } from '@/contexts/AppThemeContext'
+
+// MUI
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+
+//services
+import { logout } from '@/services'
+
 function Profile() {
-  return <></>
+  const themeContext = useContext(AppThemeContext)
+  return (
+    <>
+      <Header />
+      <Container className="mb-2" maxWidth="lg">
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={6}>
+            <CardComponent>Seus dados</CardComponent>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <CardComponent>
+              <StyledH2 className="mb-1">Definições de conta</StyledH2>
+              <StyledButton
+                className="primary mb-1"
+                onClick={themeContext?.toggleTheme}
+              >
+                Trocar para tema{' '}
+                {themeContext?.appTheme === 'light' ? 'escuro' : 'claro'}
+              </StyledButton>
+              <StyledButton className="alert" onClick={logout}>
+                logout
+              </StyledButton>
+            </CardComponent>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  )
 }
 
 export default Profile
